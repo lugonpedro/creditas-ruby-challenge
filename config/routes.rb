7826888f648c3d9df8c28ddf682ec228b7452006
Pedro Lugon <pedromotta20@gmail.com>
2024-09-10 20:59:00 -0300

    feat(): add first crud

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/articles", to: "articles#index"
+  root 'articles#index'
+  resources :articles
 end
